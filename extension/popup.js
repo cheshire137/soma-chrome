d25f546121d256b6c2cad7987da548a87d86748e
Sarah Vessels <sarah.vessels@cirrusmio.com>
2014-04-17 18:28:19 -0400

    Show/hide play/pause as necessary, enable play button

--- extension/popup.js
+++ extension/popup.js
@@ -3,7 +3,52 @@ var SomaPlayerPopup;
 SomaPlayerPopup = (function() {
   function SomaPlayerPopup() {
     console.debug('popup opened');
+    this.station_select = $('#station');
+    this.play_button = $('#play');
+    this.pause_button = $('#pause');
+    this.station_select.change((function(_this) {
+      return function() {
+        return _this.station_changed();
+      };
+    })(this));
+    this.play_button.click((function(_this) {
+      return function() {
+        return _this.play();
+      };
+    })(this));
+    this.pause_button.click((function(_this) {
+      return function() {
+        return _this.pause();
+      };
+    })(this));
   }
+
+  SomaPlayerPopup.prototype.play = function() {
+    var station;
+    station = this.station_select.val();
+    console.debug('play button clicked, station', station);
+    this.pause_button.removeClass('hidden');
+    return this.play_button.addClass('hidden');
+  };
+
+  SomaPlayerPopup.prototype.pause = function() {
+    var station;
+    station = this.station_select.val();
+    console.debug('pause button clicked, station', station);
+    this.pause_button.addClass('hidden');
+    return this.play_button.removeClass('hidden');
+  };
+
+  SomaPlayerPopup.prototype.station_changed = function() {
+    var station;
+    station = this.station_select.val();
+    console.debug('station changed to', station);
+    if (station === '') {
+      return this.play_button.attr('disabled', 'disabled');
+    } else {
+      return this.play_button.removeAttr('disabled');
+    }
+  };
 
   return SomaPlayerPopup;
 
--- src/popup.coffee
+++ src/popup.coffee
@@ -1,6 +1,35 @@
 class SomaPlayerPopup
   constructor: ->
     console.debug 'popup opened'
+    @station_select = $('#station')
+    @play_button = $('#play')
+    @pause_button = $('#pause')
+    @station_select.change =>
+      @station_changed()
+    @play_button.click =>
+      @play()
+    @pause_button.click =>
+      @pause()
+
+  play: ->
+    station = @station_select.val()
+    console.debug 'play button clicked, station', station
+    @pause_button.removeClass('hidden')
+    @play_button.addClass('hidden')
+
+  pause: ->
+    station = @station_select.val()
+    console.debug 'pause button clicked, station', station
+    @pause_button.addClass('hidden')
+    @play_button.removeClass('hidden')
+
+  station_changed: ->
+    station = @station_select.val()
+    console.debug 'station changed to', station
+    if station == ''
+      @play_button.attr('disabled', 'disabled')
+    else
+      @play_button.removeAttr('disabled')
 
 document.addEventListener 'DOMContentLoaded', ->
   new SomaPlayerPopup()
