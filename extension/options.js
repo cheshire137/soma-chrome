38df42da87028d5e7026f58a7bf0b8261d034e28
Sarah Vessels <cheshire137@gmail.com>
2015-06-20 14:20:18 -0400

    Big red Last.fm button when not connected
    Also add ability to disconnect/forget about Last.fm. Add Font Awesome
    for Last.fm icon.

--- extension/options.js
+++ extension/options.js
@@ -3,13 +3,15 @@ var SomaPlayerOptions;
 SomaPlayerOptions = (function() {
   function SomaPlayerOptions() {
     this.status_area = $('#status-message');
-    this.lastfm_button = $('button#lastfm-auth');
+    this.lastfm_button = $('button.lastfm-auth');
     this.disable_scrobbling = $('#disable_scrobbling');
     this.enable_scrobbling = $('#enable_scrobbling');
     this.disable_notifications = $('#disable_notifications');
     this.enable_notifications = $('#enable_notifications');
     this.lastfm_connected_message = $('#lastfm-is-authenticated');
+    this.lastfm_not_connected_message = $('#lastfm-is-not-authenticated');
     this.lastfm_user = $('#lastfm-user');
+    this.lastfm_disconnect = $('#lastfm-disconnect');
     this.lastfm_token = SomaPlayerUtil.get_url_param('token');
     this.options = {
       scrobbling: false,
@@ -18,6 +20,12 @@ SomaPlayerOptions = (function() {
     this.lastfm_button.click((function(_this) {
       return function() {
         return _this.init_authenticate_lastfm();
+      };
+    })(this));
+    this.lastfm_disconnect.click((function(_this) {
+      return function(event) {
+        event.preventDefault();
+        return _this.disconnect_from_lastfm();
       };
     })(this));
     $('input[name="scrobbling"]').change((function(_this) {
@@ -41,9 +49,12 @@ SomaPlayerOptions = (function() {
         if (opts.lastfm_session_key) {
           _this.lastfm_connected_message.removeClass('hidden');
           _this.enable_scrobbling.removeAttr('disabled');
+        } else {
+          _this.lastfm_not_connected_message.removeClass('hidden');
         }
         if (opts.lastfm_user) {
           _this.lastfm_user.text(opts.lastfm_user);
+          _this.lastfm_user.attr('href', "http://last.fm/user/" + opts.lastfm_user);
         }
         if (opts.scrobbling) {
           _this.enable_scrobbling.attr('checked', 'checked');
@@ -58,6 +69,28 @@ SomaPlayerOptions = (function() {
         $('.controls.hidden').removeClass('hidden');
         console.debug('SomaPlayer options:', _this.options);
         return _this.lastfm_button.removeClass('hidden');
+      };
+    })(this));
+  };
+
+  SomaPlayerOptions.prototype.disconnect_from_lastfm = function() {
+    console.debug('disconnecting from Last.fm...');
+    this.options.lastfm_session_key = null;
+    this.options.lastfm_user = null;
+    this.options.scrobbling = false;
+    return SomaPlayerUtil.set_options(this.options, (function(_this) {
+      return function() {
+        _this.status_area.text('Disconnected from Last.fm!').fadeIn(function() {
+          return setTimeout((function() {
+            return _this.status_area.fadeOut();
+          }), 2000);
+        });
+        _this.lastfm_user.text('');
+        _this.lastfm_connected_message.addClass('hidden');
+        _this.lastfm_not_connected_message.removeClass('hidden');
+        _this.enable_scrobbling.attr('disabled', 'disabled');
+        _this.enable_scrobbling.removeAttr('checked');
+        return _this.disable_scrobbling.attr('checked', 'checked');
       };
     })(this));
   };
@@ -103,6 +136,7 @@ SomaPlayerOptions = (function() {
             });
             _this.lastfm_user.text(_this.options.lastfm_user);
             _this.lastfm_connected_message.removeClass('hidden');
+            _this.lastfm_not_connected_message.addClass('hidden');
             _this.enable_scrobbling.removeAttr('disabled');
             return _this.enable_scrobbling.attr('checked', 'checked');
           });
--- src/options.coffee
+++ src/options.coffee
@@ -1,17 +1,22 @@
 class SomaPlayerOptions
   constructor: ->
     @status_area = $('#status-message')
-    @lastfm_button = $('button#lastfm-auth')
+    @lastfm_button = $('button.lastfm-auth')
     @disable_scrobbling = $('#disable_scrobbling')
     @enable_scrobbling = $('#enable_scrobbling')
     @disable_notifications = $('#disable_notifications')
     @enable_notifications = $('#enable_notifications')
     @lastfm_connected_message = $('#lastfm-is-authenticated')
+    @lastfm_not_connected_message = $('#lastfm-is-not-authenticated')
     @lastfm_user = $('#lastfm-user')
+    @lastfm_disconnect = $('#lastfm-disconnect')
     @lastfm_token = SomaPlayerUtil.get_url_param('token')
     @options = {scrobbling: false, notifications: true}
     @lastfm_button.click =>
       @init_authenticate_lastfm()
+    @lastfm_disconnect.click (event) =>
+      event.preventDefault()
+      @disconnect_from_lastfm()
     $('input[name="scrobbling"]').change =>
       @save_options()
     $('input[name="notifications"]').change =>
@@ -24,8 +29,11 @@ class SomaPlayerOptions
       if opts.lastfm_session_key
         @lastfm_connected_message.removeClass 'hidden'
         @enable_scrobbling.removeAttr 'disabled'
+      else
+        @lastfm_not_connected_message.removeClass 'hidden'
       if opts.lastfm_user
         @lastfm_user.text opts.lastfm_user
+        @lastfm_user.attr 'href', "http://last.fm/user/#{opts.lastfm_user}"
       if opts.scrobbling
         @enable_scrobbling.attr 'checked', 'checked'
       if opts.notifications == false
@@ -35,6 +43,21 @@ class SomaPlayerOptions
       $('.controls.hidden').removeClass 'hidden'
       console.debug 'SomaPlayer options:', @options
       @lastfm_button.removeClass 'hidden'
+
+  disconnect_from_lastfm: ->
+    console.debug 'disconnecting from Last.fm...'
+    @options.lastfm_session_key = null
+    @options.lastfm_user = null
+    @options.scrobbling = false
+    SomaPlayerUtil.set_options @options, =>
+      @status_area.text('Disconnected from Last.fm!').fadeIn =>
+        setTimeout (=> @status_area.fadeOut()), 2000
+      @lastfm_user.text ''
+      @lastfm_connected_message.addClass 'hidden'
+      @lastfm_not_connected_message.removeClass 'hidden'
+      @enable_scrobbling.attr 'disabled', 'disabled'
+      @enable_scrobbling.removeAttr 'checked'
+      @disable_scrobbling.attr 'checked', 'checked'
 
   save_options: ->
     @options.scrobbling = $('input[name="scrobbling"]:checked').val() == 'enabled'
@@ -62,6 +85,7 @@ class SomaPlayerOptions
             setTimeout (=> @status_area.fadeOut()), 2000
           @lastfm_user.text @options.lastfm_user
           @lastfm_connected_message.removeClass 'hidden'
+          @lastfm_not_connected_message.addClass 'hidden'
           @enable_scrobbling.removeAttr 'disabled'
           @enable_scrobbling.attr 'checked', 'checked'
       error: (data) =>
